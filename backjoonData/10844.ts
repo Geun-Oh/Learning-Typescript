e252f63612eec5a1c00362398bc02a4f0d6e919a
Geun-Oh <kandy1002@naver.com>
2022-06-21 14:41:36 +0000

    22.06.21 commit

--- backjoonData/10844.ts
+++ backjoonData/10844.ts
@@ -1,24 +1,23 @@
-"use strict";
 const fs = require("fs");
 const input = fs.readFileSync("./예제.txt").toString().trim();
 let answer = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-for (let i = 0; i < Number(input) - 1; i++) {
+
+for(let i = 0; i < Number(input) - 1; i++) {
     const temp = [...answer];
     answer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (let j = 0; j < 10; j++) {
-        if (j === 0) {
+    for(let j = 0; j < 10; j++) {
+        if(j === 0) {
             answer[1] += temp[j] % 1000000000;
-        }
-        else if (j === 9) {
+        } else if(j === 9) {
             answer[8] += temp[j] % 1000000000;
-        }
-        else {
+        } else {
             answer[j + 1] += temp[j] % 1000000000;
             answer[j - 1] += temp[j] % 1000000000;
         }
     }
 }
+
 const sum = answer.reduce((acc, curr) => {
     return (acc + curr) % 1000000000;
-}, 0);
+  }, 0);
 console.log(sum);
