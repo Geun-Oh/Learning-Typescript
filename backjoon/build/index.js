0b3e0597fdcd802138214889d7293d70d5710dc3
Geun-Oh <kandy1002@naver.com>
2022-06-27 14:35:18 +0000

    22.06.27

--- backjoon/build/index.js
+++ backjoon/build/index.js
@@ -4,14 +4,10 @@ const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
 const [n, ...nums] = input;
 const p = n.split(" ")[0];
 const q = n.split(" ")[1];
-const answer = new Array(Number(q) + 1).fill(0);
-answer[0] = 1;
+const answer = Array.from(Array(Number(p)), () => new Array(Number(q) + 1).fill(0));
 for (let i = 1; i < Number(q) + 1; i++) {
-    for (let j = 0; j < Number(p); j++) {
-        if (i - Number(nums[j]) < 0)
-            continue;
-        answer[i] += answer[i - Number(nums[j])];
-        console.log(answer[i], i);
+    if (i >= Number(nums[0].split(" ")[0])) {
+        answer[0][i] = Number(nums[0].split(" ")[1]);
     }
 }
 console.log(answer);
