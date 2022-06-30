b77d4596224032e5bc5970a2055975294ddf320f
Geun-Oh <kandy1002@naver.com>
2022-06-30 14:10:55 +0000

    22.06.30 commit

--- backjoon/build/index.js
+++ backjoon/build/index.js
@@ -1,4 +1,5 @@
 "use strict";
+let start = new Date();
 const fs = require("fs");
 const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
 const [n, ...nums] = input;
@@ -10,4 +11,16 @@ for (let i = 1; i < Number(q) + 1; i++) {
         answer[0][i] = Number(nums[0].split(" ")[1]);
     }
 }
-console.log(answer);
+for (let i = 1; i < Number(q) + 1; i++) {
+    for (let j = 1; j < Number(p); j++) {
+        if (i >= Number(nums[j].split(" ")[0])) {
+            answer[j][i] = answer[j - 1][i] > answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]) ? answer[j - 1][i] : answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]);
+        }
+        else if (i < Number(nums[j].split(" ")[0])) {
+            answer[j][i] = answer[j - 1][i];
+        }
+    }
+}
+console.log(answer[Number(p) - 1][Number(q)]);
+let end = new Date();
+console.log(Number(end) - Number(start));
--- backjoon/index.ts
+++ backjoon/index.ts
@@ -1,17 +1,22 @@
 const fs = require("fs");
 const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
 const [ n, ...nums ] =input;
-const p = n.split(" ")[0];
-const q = n.split(" ")[1];
-const answer: number[][] = Array.from(Array(Number(p)), () => new Array(Number(q) + 1).fill(0));
-for(let i = 1; i < Number(q) + 1; i++) {
+const p = Number(n.split(" ")[0]);
+const q = Number(n.split(" ")[1]);
+const answer: number[][] = Array.from(Array(p), () => new Array(q + 1).fill(0));
+for(let i = 1; i < q + 1; i++) {
     if(i >= Number(nums[0].split(" ")[0])) {
-        answer[0][i] = Number(nums[0].split(" ")[1])
+        answer[0][i] = Number(nums[0].split(" ")[1]);
     }
 }
-for(let i = 1; i < Number(q); i++) {
-    for(let j = 1; j < Number(p) + 1; j++) {
-        if(i >= Number(nums[]))
+for(let i = 1; i < q + 1; i++) {
+    for(let j = 1; j < p; j++) {
+        if(i >= Number(nums[j].split(" ")[0])) {
+            answer[j][i] = answer[j - 1][i] > answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]) ? answer[j - 1][i] : answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]);
+        }
+        else if(i < Number(nums[j].split(" ")[0])) {
+            answer[j][i] = answer[j - 1][i];
+        }
     }
 }
-console.log(answer);
+console.log(answer[p - 1][q]);
