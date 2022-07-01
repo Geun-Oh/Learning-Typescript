8116e13547ae5e1bfa3c7bcbff4f3641c219170a
Geun-Oh <kandy1002@naver.com>
2022-07-01 15:14:24 +0000

    22.07.01 commit

--- backjoon/build/index.js
+++ backjoon/build/index.js
@@ -1,26 +1,14 @@
 "use strict";
-let start = new Date();
 const fs = require("fs");
-const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
-const [n, ...nums] = input;
-const p = n.split(" ")[0];
-const q = n.split(" ")[1];
-const answer = Array.from(Array(Number(p)), () => new Array(Number(q) + 1).fill(0));
-for (let i = 1; i < Number(q) + 1; i++) {
-    if (i >= Number(nums[0].split(" ")[0])) {
-        answer[0][i] = Number(nums[0].split(" ")[1]);
-    }
-}
-for (let i = 1; i < Number(q) + 1; i++) {
-    for (let j = 1; j < Number(p); j++) {
-        if (i >= Number(nums[j].split(" ")[0])) {
-            answer[j][i] = answer[j - 1][i] > answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]) ? answer[j - 1][i] : answer[j - 1][i - Number(nums[j].split(" ")[0])] + Number(nums[j].split(" ")[1]);
-        }
-        else if (i < Number(nums[j].split(" ")[0])) {
-            answer[j][i] = answer[j - 1][i];
+const input = Number(fs.readFileSync("./예제.txt").toString().trim().split("\n"));
+let answer = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+for (let i = 0; i < input - 1; i++) {
+    const temp = [...answer];
+    answer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let j = 0; j < 10; j++) {
+        for (let k = 0; k < j + 1; k++) {
+            answer[j] += temp[j - k] % 10007;
         }
     }
 }
-console.log(answer[Number(p) - 1][Number(q)]);
-let end = new Date();
-console.log(Number(end) - Number(start));
+console.log(answer.reduce((a, b) => a + b) % 10007);
