e08562c228508b7b585395cd6901d081366e5ef2
Geun-Oh <kandy1002@naver.com>
2022-07-10 07:56:17 +0000

    22.07.10 commit

--- backjoon/build/index.js
+++ backjoon/build/index.js
@@ -1,23 +1,11 @@
 "use strict";
 const fs = require("fs");
-const input = fs.readFileSync("./예제.txt").toString().trim().split(" ");
-const n = Number(input[0]);
-let m = Number(input[1]);
-let count = 1;
-while (n < m) {
-    if (m % 10 === 1) {
-        m = Math.floor(m / 10);
-    }
-    else if (m % 2 === 0) {
-        m = m / 2;
-    }
-    else {
-        count = -1;
-        break;
-    }
-    count++;
-}
-if (n !== m) {
-    count = -1;
-}
-console.log(m, count);
+const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
+const [n, ...nums] = input;
+const arr = [...nums];
+const Arr = arr.map(x => Number(x));
+let answer = Arr.filter((item, index) => index < 3);
+answer = answer.filter(x => x !== Math.min(...answer));
+// for(let i = 3; i < Number(n); i++) {
+// }
+console.log(answer);
--- backjoon/index.ts
+++ backjoon/index.ts
@@ -1,23 +1,28 @@
 const fs = require("fs");
-const input = fs.readFileSync("./예제.txt").toString().trim().split(" ");
-const n = Number(input[0]);
-let m = Number(input[1]);
-let count = 1;
+const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
+const [ n, ...nums ] = input;
+const arr = [...nums];
+const Arr = arr.map(x => Number(x));
+let answer: number[] = Arr.filter((item, index) => index < 3);
+answer = answer.filter(x => x !== Math.min(...answer));
 
-while(n < m) {
-    if(m % 10 === 1) {
-        m = Math.floor(m / 10);
-    } else if(m % 2 === 0) {
-        m = m / 2;
-    } else {
-        count = -1;
-        break;
+for(let i = 3; i < Number(n); i++) {
+    if(answer[answer.length - 1] === Arr[i - 1] && answer[answer.length - 2] === Arr[i - 2]) {
+
     }
-    count++;
 }
 
-if(n !== m) {
-    count = -1;
-}
+console.log(answer);
 
-console.log(count);
+/**
+ * 2 5 8 4 3 5 7 2 1 8
+
+만일 맨 뒤의 두 수가 선택된 경우라면
+그대로 유지를 하거나
+해당 수를 선택할 수 있다.
+해당 수를 선택할 때
+2 인덱스 앞의 수를 택한다면
+1 인덱스 앞의 수를 택하거나
+
+5 8 3 7 이 최고였는데 여기에 2가 추가되는 것
+ */
