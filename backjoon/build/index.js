948d759031fb13bc317515149f1477c9b40b76fd
Geun-Oh <kandy1002@naver.com>
2022-07-12 14:38:46 +0000

    22.07.12 commit

--- backjoon/build/index.js
+++ backjoon/build/index.js
@@ -1,26 +1,59 @@
 "use strict";
 const fs = require("fs");
-const input = fs.readFileSync("./예제.txt").toString().trim().split("\n");
-const n = Number(input[0]);
-const m = Number(input[1]);
-const answer = Array.from(Array(n + 2), () => Array(m + 2).fill(0));
-for (let i = 0; i < n + 1; i++) {
-    answer[i][0] = 1;
-    answer[i][1] = i;
-}
-answer[4][2] = 2;
-answer[5][2] = 5;
-for (let i = 4; i < n + 1; i++) {
-    for (let j = 2; j < m + 1; j++) {
-        if (i / j < 2) {
-            continue;
-        }
-        else if (i / j === 2) {
-            answer[i][j] = 2;
-        }
-        else {
-            answer[i][j] = (answer[i - 1][j] + answer[i - 3][j - 1] + answer[i - 5][j - 2]) % 1000000003;
-        }
+const input = fs.readFileSync("./예제.txt").toString().trim().split(" ");
+const arr = input.map(x => Number(x));
+let lf = 0;
+let rf = 0;
+const sol = () => {
+    let answer = [];
+    if (arr[arr.length - 1] !== 0)
+        return 0;
+    if (arr.length === 1)
+        return 0;
+    if (arr[0] === arr[1]) {
+        answer[0] = 2;
+        answer[1] = 3;
+        lf = arr[0];
     }
-}
-console.log(answer[n][m] % 1000000003);
+    else {
+        answer[0] = 2;
+        answer[1] = 4;
+        lf = arr[0];
+        rf = arr[1];
+    }
+    if (arr.length === 2) {
+        return answer[0];
+    }
+    else if (arr.length === 3) {
+        return answer[1];
+    }
+    else if (arr.length > 3) {
+        for (let i = 2; i < arr.length - 1; i++) {
+            if (arr[i] === lf || arr[i] === rf) {
+                answer[i] = answer[i - 1] + 1;
+            }
+            else if (Math.abs(arr[i] - lf) === 2) {
+                answer[i] = answer[i - 1] + 3;
+                rf = arr[i];
+            }
+            else if (Math.abs(arr[i] - rf) === 2) {
+                answer[i] = answer[i - 1] + 3;
+                lf = arr[i];
+            }
+            else if (Math.abs(lf - rf) === 2) {
+                answer[i] = answer[i - 1] + 3;
+                if (arr[i + 1] === lf) {
+                    rf = arr[i];
+                }
+                else if (arr[i + 1] === rf) {
+                    lf = arr[i];
+                }
+                else {
+                    lf = arr[i];
+                }
+            }
+        }
+        return answer[answer.length - 1];
+    }
+};
+console.log(sol());
